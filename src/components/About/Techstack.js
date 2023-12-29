26cee3db6f86f3d97fdf5f6fb4de443eb162c427
Rajasekhar Betha <betha.rajasekhar@outlook.com>
2023-12-29 19:32:15 +0530

    Updated all my details

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,25 +1,6 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import { CgCPlusPlus } from "react-icons/cg";
-import { AiOutlineAmazon } from 'react-icons/ai';
-import { BsCloud } from 'react-icons/bs';
-import {
-  DiJavascript1,
-  DiReact,
-  DiNodejs,
-  DiMongodb,
-  DiPython,
-  DiGit,
-  DiJava,
-} from "react-icons/di";
-import {
-  SiRedis,
-  SiFirebase,
-  SiNextdotjs,
-  SiSolidity,
-  SiPostgresql,
-} from "react-icons/si";
-import { TbBrandGolang } from "react-icons/tb";
+
 
 function Techstack() {
   return (
